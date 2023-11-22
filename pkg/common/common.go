a6ffa47108b1f743b9ede36d7f3da4c2006321aa
BradHacker <harkerbd@gmail.com>
2023-11-21 20:21:48 -0500

    Define deploy state in common gRPC

---
+++ pkg/common/common.go
@@ -0,0 +1,8 @@
+package common
+
+const (
+	DeployStateFAILED     string = "FAILED"
+	DeployStateSUCCEEDED  string = "SUCCEEDED"
+	DeployStateINPROGRESS string = "INPROGRESS"
+	DeployStateDESTROYED  string = "DESTROYED"
+)
